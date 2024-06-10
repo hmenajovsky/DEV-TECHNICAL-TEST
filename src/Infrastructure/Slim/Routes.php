a51e09868370e37a4399594df5719c9a2212a543
equipe <equipe@adventium.fr>
2024-06-10 18:34:16 +0200

    Fix routes.php file

--- src/Infrastructure/Slim/Routes.php
+++ src/Infrastructure/Slim/Routes.php
@@ -3,9 +3,10 @@
 namespace EneraTechTest\Infrastructure\Slim;
 
 use EneraTechTest\Adapters\API\GetBooks\Controller as APIGetBooksController;
-use EneraTechTest\Adapters\API\PatchBook\Controller as APIPatchController;
+
 use EneraTechTest\Adapters\API\DeleteBook\Controller as APIDeleteController;
 use EneraTechTest\Adapters\API\PostBook\Controller as Controller;
+
 
 class Routes
 {
@@ -13,21 +14,26 @@ class Routes
     {
         $app->get('/api/books', APIGetBooksController::class);
         $app->post('/api/books', Controller::class);
-        $app->get('/api/books/{bookID}', APIGetBooksController::class);
-        $app->get('/', function ($request, $response) {
-            $html = file_get_contents(dirname(__DIR__, 3) . '/public/books.html');
-            $response->getBody()->write($html);
+        $app->get('/api/books/{bookID}', APIGetBooksController::class);   
+        $app->get('/', function ($request, $response) {                   
+            $html = file_get_contents(dirname(__DIR__, 3). '/public/books.html');            
+            $response->getBody()->write($html);                                         
             return $response;
-        });
-        $app->get('/js/{file:.*}', function ($request, $response, $args) {
-            $filePath = dirname(__DIR__, 3) . '/public/js/' . $args['file'];
+        });     
+        $app->get('/{file:.*}', function ($request, $response) {                   
+            $html = file_get_contents(dirname(__DIR__, 3). '/public/404.html');            
+            $response->getBody()->write($html);                                         
+            return $response;
+        });         
+       $app->get('/js/{file:.*}', function ($request, $response, $args) {            
+            $filePath = dirname(__DIR__, 3). '/public/js/' . $args['file'];
             if (file_exists($filePath)) {
                 return $response->withHeader('Content-Type', 'application/javascript')
-                    ->write(file_get_contents($filePath));
+                                ->write(file_get_contents($filePath));
             } else {
                 return $response->withStatus(404, 'Fichier non trouvé');
-            }            
-        });
+            }
+        });            
         $app->patch('/api/books/{bookID}', function ($request, $response) {
             $bookId = $request->getAttribute('bookID');
             $data = $request->getParsedBody();
@@ -53,7 +59,7 @@ class Routes
             } else {
                 return $response->withStatus(400)->withJson(['error' => 'Champ "book.title" ou "book.releaseDate" manquant dans la requête']);
             }
-        });
-        $app->delete('/books/{id}', APIDeleteController::class);
+        });   
+        $app->delete('/api/books/{bookID}', APIDeleteController::class);   
     }
 }
